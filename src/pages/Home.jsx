934687ccfba4213187d3c9d256c6b8238f14ce42
faysal hossain <devfaysalbd@gamil.com>
2025-11-10 09:20:35 +0600

    some update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router';
 import Bids from './Bids';
+import Banner from '../components/Banner';
 
 const Home = () => {
     return (
         <>
+        {/* banner component */}
+        {/* <Banner /> */}
+        {/* banner component */}
             {/* <!-- HERO (Banner) --> */}
             <section id="home" className="hero-bg text-white">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 lg:py-28">
