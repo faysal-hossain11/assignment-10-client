125c9178e1ffd853962fb1377adea686b648e6f7
faysal hossain <devfaysalbd@gamil.com>
2025-11-13 17:30:11 +0600

    manamge .env file

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import process from 'process'; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCX3LM36LpZlvoLBtncRNkR8SSzUKK2Pcw",
-  authDomain: "smart-deals-aa5d4.firebaseapp.com",
-  projectId: "smart-deals-aa5d4",
-  storageBucket: "smart-deals-aa5d4.firebasestorage.app",
-  messagingSenderId: "10285931677",
-  appId: "1:10285931677:web:0037499f95e948d8630874"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
